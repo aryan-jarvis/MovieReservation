38e2de256fca72d3b27a948b279b19f9dd2cd9b5
aryan-jarvis <aryan.doshi@jarvis.consulting>
2025-07-02 11:17:17 +0530

    t6

--- backend/models/setup.go
+++ backend/models/setup.go
@@ -1,7 +1,9 @@
 package models
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -10,15 +12,15 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	dsn := "host=localhost user=goauthuser password=password dbname=auth_db port=5432 sslmode=disable"
-	// dsn := fmt.Sprintf(
-	// 	"host=%s user=%s password=%s dbname=%s port=%s sslmode=require TimeZone=UTC",
-	// 	os.Getenv("DB_HOST"),
-	// 	os.Getenv("DB_USER"),
-	// 	os.Getenv("DB_PASSWORD"),
-	// 	os.Getenv("DB_NAME"),
-	// 	os.Getenv("DB_PORT"),
-	// )
+	// dsn := "host=localhost user=goauthuser password=password dbname=auth_db port=5432 sslmode=disable"
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=require TimeZone=UTC",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
 
 	// dsn := "host=dpg-d1iba1ripnbc73beqntg-a.oregon-postgres.render.com user=moviedb_et2q_user password=2xHxOTQ40oy0KLVQiJPqf9VWK471619V dbname=moviedb_et2q port=5432 sslmode=require TimeZone=UTC"
 
