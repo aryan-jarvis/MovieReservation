b548ec77b80cf49b99a5e509653a26cfd8bab6f6
aryan-jarvis <aryan.doshi@jarvis.consulting>
2025-07-02 10:45:09 +0530

    t5

--- backend/main.go
+++ backend/main.go
@@ -26,7 +26,13 @@ func init() {
 		"PAYU_MERCHANT_KEY",
 		"PAYU_MERCHANT_SALT",
 		"PAYU_BASE_URL",
-		"DATABASE_URL", // <— add this
+
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=require TimeZone=UTC",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
 	}
 	for _, key := range requiredEnv {
 		if os.Getenv(key) == "" {
